d730a4efdef3ad08816e05583643dbd05cdc2814
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-25 01:56:21 +1000

    Test that searchSetText actually gives non-empty results.

--- tests/testthat/test-searchSetText.R
+++ tests/testthat/test-searchSetText.R
@@ -27,10 +27,12 @@ test_that("basic search works for the remote ref", {
     flushMemoryCache()
 
     cout <- searchSetText("9606", "cancer")
+    expect_gt(length(cout), 0L)
     expect_true(all(grepl("cancer", sets$name[cout], ignore.case=TRUE) | grepl("cancer", sets$description[cout], ignore.case=TRUE)))
 
     # Multiple words with a prefixed wildcard.
     iout <- searchSetText("9606", "innate immun*")
+    expect_gt(length(iout), 0L)
     expect_true(
         all(
             (grepl("innate", sets$name[iout], ignore.case=TRUE) | grepl("innate", sets$description[iout], ignore.case=TRUE)) &
@@ -39,6 +41,7 @@ test_that("basic search works for the remote ref", {
     )
 
     aout <- searchSetText("9606", "adaptive immun*")
+    expect_gt(length(aout), 0L)
     expect_true(
         all(
             (grepl("adaptive", sets$name[aout], ignore.case=TRUE) | grepl("adaptive*", sets$description[aout], ignore.case=TRUE)) &
@@ -48,9 +51,11 @@ test_that("basic search works for the remote ref", {
 
     # Trying with only the name or description (also tests the cache).
     iout.noname <- searchSetText("9606", "immun*", use.name=FALSE)
+    expect_gt(length(iout.noname), 0L)
     expect_true(all(grepl("immun.*", sets$description[iout.noname], ignore.case=TRUE)))
 
     iout.nodesc <- searchSetText("9606", "immun*", use.description=FALSE)
+    expect_gt(length(iout.nodesc), 0L)
     expect_true(all(grepl("immun.*", sets$name[iout.nodesc], ignore.case=TRUE)))
 
     iout.none <- searchSetText("9606", "immun*", use.description=FALSE, use.name=FALSE)
@@ -58,9 +63,11 @@ test_that("basic search works for the remote ref", {
 
     # Non-prefixed wildcard.
     nout <- searchSetText("9606", "*nucleic*")
+    expect_gt(length(nout), 0L)
     expect_true(all(grepl(".*nucleic.*", sets$name[nout], ignore.case=TRUE) | grepl(".*nucleic.*", sets$description[nout], ignore.case=TRUE)))
 
     # Trying a rare question mark.
     tout <- searchSetText("9606", "?typical")
+    expect_gt(length(tout), 0L)
     expect_true(all(grepl(".typical", sets$name[tout], ignore.case=TRUE) | grepl(".typical", sets$description[tout], ignore.case=TRUE)))
 })
